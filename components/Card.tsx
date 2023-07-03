782bac449fcaac499b414e68312cd2e25aa568ea
Tengoku18 <tengokusenpai18@gmail.com>
2023-07-03 10:39:42 +0545

    add : added gallery section

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,38 +1,50 @@
 /* eslint-disable @next/next/no-img-element */
-import Link from 'next/link';
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import {
+  BeakerIcon,
+  MusicalNoteIcon,
+  FireIcon,
+} from "@heroicons/react/24/solid";
+import Image from "next/image";
 
 type Props = {
-    imagUrl : string;
-}
+  imagUrl: string;
+  albumName?: string;
+  releaseDate?: any;
+  popularity?: string;
+};
 
-const Card = ({ imagUrl }: Props) => {
+const Card = ({ imagUrl, albumName, releaseDate, popularity }: Props) => {
   return (
-    
-<div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <Link href="/">
-        <img className="p-8 rounded-t-lg" src={imagUrl} alt="product image" />
-    </Link>
-    <div className="px-5 pb-5">
-        <Link href="/">
-            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</h5>
-        </Link>
-        <div className="flex items-center mt-2.5 mb-5">
-            <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-            <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Second star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-            <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Third star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-            <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fourth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-            <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fifth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-            <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
+    <div className="w-full max-w-sm bg-white border border-gray-200 rounded-sm shadow dark:bg-gray-800 dark:border-gray-700">
+      <Image
+        height={300}
+        width={400}
+        className="p-4 rounded-t-lg"
+        src={imagUrl}
+        alt="product image"
+      />
+
+      <div className="px-5 pb-5 w-full max-w-sm">
+        <div className="flex justify-between">
+          <div className="flex">
+            <MusicalNoteIcon className="h-6 w-6 mr-2  text-gray-600 " />{" "}
+            <h3 className="text-md font-bold line-clamp-1 w-40 ">
+              {" "}
+              {albumName}{" "}
+            </h3>
+          </div>
+          {popularity && (
+            <div className="flex">
+              <FireIcon className="h-6 w-6 mr-2  text-gray-600 " />{" "}
+              <h3 className="text-md font-bold"> {popularity} </h3>
+            </div>
+          )}
         </div>
-        <div className="flex items-center justify-between">
-            <span className="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
-            <Link href="/" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</Link>
-        </div>
+      </div>
     </div>
-</div>
+  );
+};
 
-  )
-}
-
-export default Card
+export default Card;
--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,54 +1,103 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react'
-import Card from './Card'
 
-type Props = {}
+import React, { useEffect, useState } from "react";
+import Card from "./Card";
+import axios from "axios";
+import Link from "next/link";
+import { useQuery } from "@tanstack/react-query";
+import Spinner from "./Spinner";
+import { useDispatch, useSelector } from "react-redux";
+import HeaderWithButton from "./HeaderWithButton";
 
-const Gallery = (props: Props) => {
+const Gallery = () => {
+  const searchedAlbum = useSelector((state) => state.allAlbum.album);
+
+  // console.log("query is sdsdds", storeAlbum);
+
+  const getAlbums = async () => {
+    const response = await axios.get(
+      "https://spotify23.p.rapidapi.com/albums/",
+      {
+        params: {
+          ids: "1qHht0Qw9uUWMWHnqObZ7g,2lBnYvt0Yf3HORrdVAVLte,7pG7UD4ytFwvZYziuKKasJ,23bqeyhRwGWrVJEJ00KXDP,5l5gR4rh26QI3fijGFTDrp,3IBcauSj5M2A6lTeffJzdv,2eQMC9nJE3f3hCNKlYYHL1,2rb6C1wUwk7hFOVmfgt19k,6T3JpPBkz5CYJlwtXBofkl,5amj9zNeZ3B2EdpBgXrOZ0,1DF9B2hfwX4EdgEFwGcRwh",
+        },
+        headers: {
+          "X-RapidAPI-Key":
+            "37149707ebmsh7abd441c54bb7f1p147a1fjsn0aa037309cc2",
+          "X-RapidAPI-Host": "spotify23.p.rapidapi.com",
+        },
+      }
+    );
+    const { data } = response;
+    return data.albums;
+    // setAlbums(data.albums);
+  };
+
+  const {
+    data: trendingAlbums,
+    error,
+    isLoading,
+  } = useQuery(["fetchAlbums"], getAlbums, { staleTime: 10000 });
+
+  if (isLoading) {
+    return (
+      <div className="h-[80vh] flex justify-center items-center ">
+        {" "}
+        <Spinner />{" "}
+      </div>
+    );
+  }
+  if (error) {
+    return (
+      <h1 className="text-center text-xl ">
+        {" "}
+        An error has occured {error?.message}{" "}
+      </h1>
+    );
+  }
+
   return (
-  
-<main className=" max-w-6xl mx-auto py-10 grid grid-cols-2 md:grid-cols-3 gap-4">
-    <div>
-       
-      
-        <Card  imagUrl="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg"  />
-    </div>
-    <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="" />
-    </div>
-    <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt="" />
-    </div>
-    <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt="" />
-    </div>
-    <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" alt="" />
-    </div>
-    <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg" alt="" />
-    </div>
-    <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg" alt="" />
-    </div>
-    <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg" alt="" />
-    </div>
-    <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg" alt="" />
-    </div>
-    <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg" alt="" />
-    </div>
-    <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg" alt="" />
-    </div>
-    <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg" alt="" />
-    </div>
-</main>
+    <main className="max-w-6xl mx-auto px-3 md:px-5 flex flex-col">
+      <HeaderWithButton
+        title={
+          searchedAlbum.length > 0 ? "Searched Results " : "Trending Albums"
+        }
+      />
+      <div className="  pb-10 mt-3 grid place-content-center grid-cols-auto md:grid-cols-3 gap-4">
+        {searchedAlbum.length > 0
+          ? searchedAlbum.map((album, i) => {
+              const { data } = album;
+              const { name, coverArt, uri } = data;
 
-  )
-}
+              const id = uri.split(":").pop();
 
-export default Gallery
+              return (
+                <div key={uri}>
+                  <Link href={` /details/${id}`}>
+                    <Card albumName={name} imagUrl={coverArt.sources[0].url} />
+                  </Link>
+                </div>
+              );
+            })
+          : trendingAlbums &&
+            trendingAlbums.map((album: any, i: any) => {
+              const { name, popularity, uri, images, id } = album;
+
+              return (
+                <div key={uri}>
+                  <Link href={` /details/${id}`}>
+                    <Card
+                      albumName={name}
+                      popularity={popularity}
+                      imagUrl={images[1].url}
+                    />
+                  </Link>
+                </div>
+              );
+            })}
+      </div>
+    </main>
+  );
+};
+
+export default Gallery;
