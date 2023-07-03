50c8389777f2cce3fafbd66e9142271876eee77c
Tengoku18 <tengokusenpai18@gmail.com>
2023-07-03 10:40:18 +0545

    Some changes on layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,30 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+"use client";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Music Sansar',
-  description: 'Sansar for music lovers',
-}
+  title: "Music Sansar",
+  description: "Sansar for music lovers",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
+  const queryClient = new QueryClient();
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className= ' bg-slate-700 ' >
-        {children}
+        <div className=" bg-slate-700 ">
+          <QueryClientProvider client={queryClient}>
+            {children}
+          </QueryClientProvider>
         </div>
-        </body>
+      </body>
     </html>
-  )
+  );
 }
